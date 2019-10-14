5ce18aeed1ad961122bf41e65920ef45fb9a0b0b
thientinh99 <56353735+thientinh99@users.noreply.github.com>
2019-10-14 11:54:26 +0700

    tinh

---
+++ JavaFx Template/src/user/dao/DAO.java
@@ -0,0 +1,13 @@
+package user.dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+    public void add(T o);
+
+    public void remove(T o);
+
+    public void update(T o);
+
+    public List<T> getList();
+}
