d0dc3a623a27a4b0b9f5a2fba7b6dc0490951fd5
thientinh99 <56353735+thientinh99@users.noreply.github.com>
2019-10-28 19:09:34 +0700

    tinh

---
+++ 2. Application/src/application/product/addcategory/AddCategoryPresenter.java
@@ -0,0 +1,13 @@
+package application.product.addcategory;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCategoryPresenter implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
