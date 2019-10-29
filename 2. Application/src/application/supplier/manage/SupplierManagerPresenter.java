981d096da4942da0ed90401409b95b0ddfc3daed
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-10-29 21:44:50 +0700

    Supplier

---
+++ 2. Application/src/application/supplier/manage/SupplierManagerPresenter.java
@@ -0,0 +1,13 @@
+package application.supplier.manage;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SupplierManagerPresenter implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
