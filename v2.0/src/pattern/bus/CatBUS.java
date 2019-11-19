d4c69ec7fd3df8ac54babbc919e626c013217c66
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-11-19 17:14:01 +0700

    hjkhkk

--- v2.0/src/pattern/bus/CatBUS.java
+++ v2.0/src/pattern/bus/CatBUS.java
@@ -1,15 +1,5 @@
 package pattern.bus;
 
-<<<<<<< HEAD
-import pattern.dao.CatDAO;
-import pattern.model.Category;
-
-public class CatBUS implements BUS<Category> {
-    CatDAO catDAO = new CatDAO();
-    @Override
-    public void add(Category o) {
-        if(isUniqueName(o)){
-=======
 import javafx.scene.control.Alert;
 import javafx.stage.StageStyle;
 import pattern.connection.ConnectionFactory;
@@ -37,48 +27,29 @@ public class CatBUS implements BUS<Category> {
     @Override
     public void add(Category o) {
         if (isUniqName(o)) {
->>>>>>> 3ecfc4fa8522d906303f7d38e882c5fbdf94d9e4
             catDAO.add(o);
         }
     }
 
     @Override
     public void update(Category o) {
-<<<<<<< HEAD
-
-=======
         if (isTrueUpdate(o)) {
             catDAO.update(o);
         } else if (isUniqName(o)) {
             catDAO.update(o);
         }
->>>>>>> 3ecfc4fa8522d906303f7d38e882c5fbdf94d9e4
     }
 
     @Override
     public void delete(Category o) {
-<<<<<<< HEAD
-
-=======
         if (catDAO.isNotUsed(o)) {
             catDAO.remove(String.valueOf(o));//
         }
->>>>>>> 3ecfc4fa8522d906303f7d38e882c5fbdf94d9e4
     }
 
     @Override
     public boolean isTrueUpdate(Category o) {
-<<<<<<< HEAD
-        return false;
-    }
-
-    @Override
-    public boolean isUniqueName(Category o) {
-        return false;
-    }
-}
-=======
-        boolean isTreu = false;
+        boolean isTrue = false;
         System.out.println("we are in update");
         try {
             String sql = "UPDATE [dbo].[Category]" +
@@ -92,24 +63,28 @@ public class CatBUS implements BUS<Category> {
             preparedStatement.execute();
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
-                return isTreu;
+                isTrue=true;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return isTreu;
+        return isTrue;
+    }
+
+    @Override
+    public boolean isUniqueName(Category o) {
+        return false;
     }
 
     @Override
     public boolean isUniqName(Category o) {
         boolean uniqCategory = false;
-        String sql = "insert into Category (CatName, [Desc]) values (?,?)";
+        String sql = "Insert into Category (CatName, [Desc]) values (?,?)";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, o.getCatName());
             preparedStatement.setString(2, o.getDesc());
-
             preparedStatement.execute();
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -120,17 +95,14 @@ public class CatBUS implements BUS<Category> {
                 alert.setContentText("CategoryName" + "  '" + o.getCatName() + "' " + "Already exist");
                 alert.initStyle(StageStyle.UNDECORATED);
                 alert.showAndWait();
-
-
             }
             uniqCategory = true;
         } catch (SQLException e) {
             Logger.getLogger(Category.class.getName()).log(Level.SEVERE, null, e);
         }
-           return uniqCategory;
+        return uniqCategory;
     }
 }
 
 
 
->>>>>>> 3ecfc4fa8522d906303f7d38e882c5fbdf94d9e4
