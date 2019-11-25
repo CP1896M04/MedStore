9bfca713f9537faf59d690209a6ee1249d7baf01
ldthuan1995 <44872705+ldthuan1995@users.noreply.github.com>
2019-11-25 17:57:51 +0700

    oke

---
+++ v2.0/src/controller/application/report/Revenue.java
@@ -0,0 +1,67 @@
+//package controller.application.report;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ComboBox;
+//import javafx.scene.control.DatePicker;
+//import pattern.dao.StaffDAO;
+//import pattern.model.Staff;
+//
+//import java.net.URL;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.ResourceBundle;
+//
+//public class Revenue implements Initializable {
+//
+//    @FXML
+//    private ComboBox<Staff> comboboxStaffID1;
+//
+//    @FXML
+//    private DatePicker dateBegin1;
+//
+//    @FXML
+//    private DatePicker dateFinsh1;
+//
+//    @FXML
+//    private Button bntPrint1;
+//
+//    @FXML
+//    private DatePicker dateBegin2;
+//
+//    @FXML
+//    private DatePicker dateFinish2;
+//
+//    @FXML
+//    private ComboBox<Staff> comboboxStaffID2;
+//
+//    @FXML
+//    private Button bntPrint2;
+//
+//    @FXML
+//    void bntPrint1(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void bntPrint2(ActionEvent event) {
+//
+//    }
+//
+//    public void loadData(){
+//        try {
+//            StaffDAO staffDAO = new StaffDAO();
+//            List<Staff> staffs = new ArrayList<>();
+//        }catch ()
+//        {
+//
+//        };
+//
+//    }
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//
+//    }
+//}
